ea35e8f9fb4c4d0f8d9a3c7fbcd0dc968e65ece5
tetrisGatiko <jlm270901@gmail.com>
2023-08-10 23:22:38 -0300

    Se agregaron los puntos faltantes del proyecto
    puntos tales como el login register

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, './public')));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2727;
 app.listen(port, () => {
-    console.log("el servidor se conecto en el puerto 3000")
+    console.log("el servidor se conecto en el puerto 2727")
 });
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/inicio.html"));
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
 })
